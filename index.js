da7609248dae6367017a32455a7a91c8f45a4db5
devair <devairft@gmail.com>
2024-05-11 22:14:45 -0300

    criando no firebase e no postgres

--- index.js
+++ index.js
@@ -5,12 +5,11 @@
  * @param {!express:Response} res HTTP response context.
  */
 
-const functions = require("firebase-functions");
+const APP_ENDPOINT = process.env.APP_ENDPOINT || "https://localhost";
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-const APP_ENDPOINT = process.env.APP_ENDPOINT || "https://localhost";
 
 exports.authUser = async (req, res) => {
     const contentType = req.headers["content-type"];
@@ -27,45 +26,83 @@ exports.authUser = async (req, res) => {
 
     if (req.method == "POST") {
         const { name, cpf, email, phone, password } = req.body;
+
+        let customer;
+        if (cpf) {
+            customer = await pgFindUser(cpf);
+            if (!customer) {
+                customer = await pgCreateUser({ name, cpf, email, phone });
+            }
+        }
+
         let userRecord;
         try {
             userRecord = await admin.auth().getUserByEmail(email);
-            return res.status(200).json({ data: userRecord });
         } catch (err) {
             console.error(err); // o método retornar uma exception quando nao encontra o e-mail
         }
 
         try {
-            userRecord = await admin.auth().createUser({
-                email: email,
-                password: password,
-            });
+            if (!userRecord) {
+                userRecord = await admin.auth().createUser({
+                    email: email,
+                    password: password,
+                    phoneNumber: phone,
+                    displayName: name,
+                });
+            }
+
+            let token;
+            if (userRecord ) {
+                if(cpf){
+                    await admin.auth().setCustomUserClaims(userRecord.uid, { cpf });
+                    // get user with CustomUserClaims
+                    userRecord = await admin.auth().getUser(userRecord.uid);
+                }
+            }
+
+            const dataReturn = { userRecord };
+
+            return res.status(200).json(dataReturn);
         } catch (err) {
             return res.status(403).send({ message: err.message });
-        }
-
-        if (userRecord) {
-            try {
-                const response = await fetch(APP_ENDPOINT + '/customers', {
-                    method: "post",
-                    body: JSON.stringify({
-                        email,
-                        phone,
-                        cpf,
-                        name,
-                        firebaseid: userRecord.uid,
-                    }),
-                    headers: {'Content-Type': 'application/json'}
-                });
-
-                return res.status(response.status).json(response.body);
-            } catch (err) {
-                return res.status(403).send({ message: err });
-            }
         }
     } else {
         return res
             .status(403)
             .send({ message: `Method not ${req.method} allowed` });
     }
+};
+
+pgCreateUser = async ({ name, cpf, email, phone }) => {
+    const response = await fetch(APP_ENDPOINT + "/customers", {
+        method: "post",
+        body: JSON.stringify({
+            email,
+            phone,
+            cpf,
+            name,
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+        return response.body;
+    }
+    return null;
+};
+
+pgFindUser = async (cpf) => {
+    const response = await fetch(
+        APP_ENDPOINT + "/customers/search?cpf=" + cpf,
+        {
+            method: "get",
+            headers: { "Content-Type": "application/json" },
+        }
+    );
+
+    if (response.ok) {
+        return response.body;
+    }
+    return null;
 };
